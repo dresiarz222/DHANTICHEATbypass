c4cfaf805d0b58804244b955256b3363cf578595
dresiarz222 <79806016+dresiarz222@users.noreply.github.com>
2024-07-27 22:41:15 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,15 @@
+local acFlags = {
+    "CHECKER_1",
+    "CHECKER_2",
+    "VirusCough",
+    "TeleportDetect",
+    "OneMoreTime",
+    "BreathingHAMON",
+}
+local old; old = hookmetamethod(game,"__namecall",function(...)
+    local args = {...}
+    if table.find(acFlags,args[2]) then
+        return
+    end
+    return old(...)
+end)
